8ef44a8a628b6d9fc80261791b37d1ce6c9baf52
mylesw27 <myleswiegel27@gmail.com>
2023-06-29 10:52:27 -0700

    update axios request and card component to match backend data

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import MatchupCard from './components/partials/MatchupCard'
 import { data } from './seedData'
+import axios from 'axios'
 
 function App() {
-  const [games] = useState(data.games)
+  const [games, setGames] = useState(data.games)
+  
+  useEffect(() => {
+    const getTeams = async () => {
+      const res = await axios.get('http://localhost:8000/scheduledata')
+      setGames(res.data)
+    }
+    getTeams()
+  }, [])
 
+  
   const matchupArray = games.map((game, i) => {
     return (
-      <MatchupCard key={`game${i}`} game={i} />
+      <MatchupCard key={`game${i}`} game={game} />
     )
   })
+
 
   return (
     <>
--- src/components/partials/MatchupCard.tsx
+++ src/components/partials/MatchupCard.tsx
@@ -1,26 +1,29 @@
-import { data } from '../../seedData'
 import './MatchupCard.css'
 
-export default function MatchupCard(props: { game: number }) {
-    const matchup = data.games[props.game]
+export default function MatchupCard(props: { game: object }) {
+    const game = props.game
+    const awayTeam = game.schedule_away_team
+    const homeTeam = game.schedule_home_team
+    const location = game.schedule_location
+
 
     return (
         <>
         <div className="matchupCard">
-            <h2>Matchup {props.game + 1}</h2>
-            <p>{matchup.location}</p>
-            <p>{matchup.stadium}</p>
+            <h2>{game.schedule_date}</h2>
+            <p>{location.location_city}, {location.location_state_abbr}</p>
+            <p>{location.stadium_name}</p>
             <div className='teams'>
-                <div className="teamCard" style={{backgroundColor: matchup.awayTeam.color1, color: matchup.awayTeam.color2}}>
-                    <p>{matchup.awayTeam.teamLocation}</p>
-                    <h3>{matchup.awayTeam.teamName}</h3>
+                <div className="teamCard" style={{backgroundColor: awayTeam.team_color_1, color: awayTeam.team_color_2}}>
+                    <p>{awayTeam.team_location}</p>
+                    <h3>{awayTeam.team_name}</h3>
                 </div>
                 <p className='at'>@</p>
-                <div className="teamCard" style={{backgroundColor: matchup.homeTeam.color1, color: matchup.homeTeam.color2}}>
-                    <p>{matchup.homeTeam.teamLocation}</p>
-                    <h3>{matchup.homeTeam.teamName}</h3>
+                <div className="teamCard" style={{backgroundColor: homeTeam.team_color_1, color: homeTeam.team_color_2}}>
+                    <p>{homeTeam.team_location}</p>
+                    <h3>{homeTeam.team_name}</h3>
                 </div>
-            </div>
+            </div> 
         </div>
         </>
     )
