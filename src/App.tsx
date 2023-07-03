aa23f6d5b30d14647a0d0a3fa6709a69f98ad9e4
mylesw27 <myleswiegel27@gmail.com>
2023-07-03 08:29:25 -0700

    add route for picks pages per weekly schedule

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import './App.css'
 import MatchupCard from './components/partials/MatchupCard'
 import { data } from './seedData'
 import axios from 'axios'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './components/pages/Home'
+import Picks from './components/pages/Picks'
 
 function App() {
   const [games, setGames] = useState(data.games)
@@ -25,8 +28,18 @@ function App() {
 
   return (
     <>
-      <h1>NFL Pick 'Em</h1>
-      {matchupArray}
+      <Router >
+        <Routes>
+          <Route 
+            path='/' 
+            element={<Home />} 
+            />
+          <Route 
+            path='/schedule/:week' 
+            element={<Picks />} 
+            />
+        </Routes>
+      </Router>
     </>
   )
 }
