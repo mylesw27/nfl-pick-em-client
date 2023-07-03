e6a1f3bd3123f4c4d3c6f3610bb03aa9438348f9
mylesw27 <myleswiegel27@gmail.com>
2023-07-03 09:41:33 -0700

    cleanup app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useState, useEffect } from 'react'
 import './App.css'
-import MatchupCard from './components/partials/MatchupCard'
-import { data } from './seedData'
-import axios from 'axios'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Picks from './components/pages/Picks'
 
 function App() {
-  const [games, setGames] = useState(data.games)
-  
-  useEffect(() => {
-    const getTeams = async () => {
-      const res = await axios.get('http://localhost:8000/scheduledata')
-      setGames(res.data)
-    }
-    getTeams()
-  }, [])
-
-  
-  const matchupArray = games.map((game, i) => {
-    return (
-      <MatchupCard key={`game${i}`} game={game} />
-    )
-  })
-
-
   return (
     <>
       <Router >
